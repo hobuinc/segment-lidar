8fb5ea03d1c27b0a936d4f5ff7c5155cc91884d8
Thomas M Donnelly <58053836+TomTheTonk@users.noreply.github.com>
2023-07-20 09:31:12 -0500

    Removed tile.tif

--- seg.py
+++ seg.py
@@ -1,7 +1,9 @@
 import os
+from posixpath import splitext
 import pdal
 import time
 from segment_lidar import samlidar
+
 
 model = samlidar.SamLidar(ckpt_path="seg-lidar/sam_vit_h_4b8939.pth")
 inDir = "tiles/"
@@ -16,15 +18,15 @@ filters = [outlier, sort]
 
 for tile in os.listdir(inDir):
     if (not tile.startswith('.')):
+        file = os.path.splitext(tile)[0]
         start=time.time()
         print(inDir+tile)
         dtypes, points, pdal_points = model.read(inDir+tile)
-        breakpoint()
-        #pdal_points = model.applyFilters(pdal_points, [outlier])
+        pdal_points = model.applyFilters(pdal_points, [outlier])
         cloud, non_ground, ground, pdal_points= model.smrf(pdal_points)
         labels, *_ = model.segment(points=cloud, image_path=outRaster+tile+"-raster.tif", labels_path=outLabels+tile+"-labeled.tif")
         points_grouped = model.grouping(pdal_points, labels, ground, non_ground)
-        pdal_points = model.featureFilter(points_grouped)
-        model.write(points=points, non_ground=non_ground, ground=ground, segment_ids=labels, save_path=outSeg+tile+"-segmented.las")
+        pdal_points = model.featureFilter(points_grouped, file)
+        model.write_pdal(points=points, non_ground=non_ground, ground=ground, segment_ids=labels, save_path=outSeg+tile+"-segmented.las")
         end = time.time()
         print(f'Segment-lidar completed in {end - start:.2f} seconds.\n')
